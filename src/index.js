a8b3a92778f8f6b32d96589483fd14448e77876a
Sanzia7 <meritee@gmx.de>
2023-05-06 11:13:30 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom'
+///import { BrowserRouter as Router, createHashRouter } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([
+    {
+        path: '/*',
+        element: <App />
+    }
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <RouterProvider router={router}/>
 
-    <Router>
-        <App />
-    </Router>
 );
 
 
